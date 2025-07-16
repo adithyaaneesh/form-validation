5ac2eeb83779fd32c9afee402507a7ce1da5db54
Adithya <adithyaaneesh29@gmail>
2025-07-16 10:14:22 +0530

    input validation

--- script.js
+++ script.js
@@ -8,21 +8,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirmPassword").value;
 
-    if (!name || !email || !password || !confirmPassword) {
-      document.getElementsByTagName('form') = "Please fill all the blanks"
+    if (!name || !email || !password ) {
+      document.getElementById("errorText").innerHTML = "Please fill all the blanks*"
+      document.getElementById("errorText").style.color = "red";
       return;
     }
+
     if (name.length>10){
       document.getElementById('user').innerHTML = "invalid name"
       return;
     }
-    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$f/;
+
+    // let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$f/;
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
     if (regex.test(email)) {
       document.getElementById('mail').innerHTML = "invalid mail address"
       return;
     }
+
     if (password !== confirmPassword) {
       document.getElementById('demo').innerHTML = "Password do not match"
+      return;
+    }
+    if (password.length<6){
+      document.getElementById('demo').innerHTML = "Password must be more than 6 character"
       return;
     }
 
